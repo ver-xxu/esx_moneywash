1dba496f193ce2b3975614a2f5e6b30898419c31
Verxxu <66355838+Verxxu@users.noreply.github.com>
2021-03-03 01:25:01 +0200

    Update wash_s.lua

--- wash_s.lua
+++ wash_s.lua
@@ -5,11 +5,21 @@ local Washers = {}
 local WashPos = vector3(0.0, 0.0, 0.0)
 -- Real position here ^^
 RegisterServerEvent("money_wash:start")
-AddEventHandler("money_wash:start", function(pos)
+AddEventHandler("money_wash:start", function()
      local _source = source
      local xPlayer = ESX.GetPlayerFromId(_source)
-     if xPlayer.getCoords
-
-
-
+     local coords = xPlayer.getCoords(true)
+     local distance = #(coords - WashPos)
+     if distance < 3 then
+        if not Washers[_source] then
+            Washers[_source] = true
+            StartWash(xPlayer)
+        end
+     else
+        print(string.format("%s is washing %s units away from the real spot", xPlayer.name, distance))
+     end
 end)
+
+StartWash = function(xPlayer)  
+
+end
