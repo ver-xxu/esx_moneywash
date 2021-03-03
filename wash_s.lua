0655f7b6028bd800061d0b0f18f615010ba0da29
Verxxu <66355838+Verxxu@users.noreply.github.com>
2021-03-03 15:31:37 +0200

    Update wash_s.lua

--- wash_s.lua
+++ wash_s.lua
@@ -1,9 +1,12 @@
-ESX = nil
+ ESX = nil
 
 TriggerEvent("esx:getSharedObject", function(asfsas) ESX = asfsas end)
+
 local Washers = {}
+
 local WashPos = vector3(0.0, 0.0, 0.0)
 -- Real position here ^^
+
 RegisterServerEvent("money_wash:start")
 AddEventHandler("money_wash:start", function()
      local _source = source
@@ -15,31 +18,34 @@ AddEventHandler("money_wash:start", function()
             Washers[_source] = true
             StartWash(xPlayer)
         end
-     else
-        print(string.format("%s is washing %s units away from the real spot", xPlayer.name, distance))
-     end
+    else
+        print(string.format("%s is trying to wash %s units away from the real spot", xPlayer.name, distance))
+    end
 end)
 
 StartWash = function(xPlayer)  
-     local money = xPlayer.getAccount("black_money").money
-     if Washers[xPlayer.source] then
-     SetTimeout(2500, function()
-         if money >= 500 then
-            xPlayer.addMoney(500)
-            xPlayer.removeAccountMoney("black_money", 500)
-            StartWash(xPlayer)
-         end
-     end)
+    local money = xPlayer.getAccount("black_money").money
+    if Washers[xPlayer.source] then
+        SetTimeout(2500, function()
+            if money >= 500 then
+                xPlayer.addMoney(500)
+                xPlayer.removeAccountMoney("black_money", 500)
+				StartWash(xPlayer)
+            end
+		end)
+	end
 end
 
 RegisterServerEvent("money_wash:getpos")
 AddEventHandler("money_wash:getpos", function()
-     local _source = source
-     TriggerClientEvent("money_wash:position", _source, WashPos)   
+    local _source = source
+    TriggerClientEvent("money_wash:position", _source, WashPos)   
 end)
 
 RegisterServerEvent("money_wash:stopwash")
 AddEventHandler("money_wash:stopwash", function()
-     local _source = source
-     Washers[_source] = nil
+    local _source = source
+    Washers[_source] = nil
 end)
+
+
